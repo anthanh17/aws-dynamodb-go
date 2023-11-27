5bc021b6651c004d3a1415b127af08b21da29cd2
An Nguyen The <92008721+anthanh17@users.noreply.github.com>
2023-11-27 11:05:18 +0700

    Create ReadItem.go

---
+++ ReadItem.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+
+	"fmt"
+	"log"
+)
+
+// Create struct to hold info about new item
+type Item struct {
+	Year   int
+	Title  string
+	Plot   string
+	Rating float64
+}
+
+func main() {
+
+	// and region from the shared configuration file ~/.aws/config.
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	// Create DynamoDB client
+	svc := dynamodb.New(sess)
+
+	tableName := "Movies"
+	movieName := "The Big New Movie"
+	movieYear := "2016"
+
+	result, err := svc.GetItem(&dynamodb.GetItemInput{
+		TableName: aws.String(tableName),
+		Key: map[string]*dynamodb.AttributeValue{
+			"Year": {
+				N: aws.String(movieYear),
+			},
+			"Title": {
+				S: aws.String(movieName),
+			},
+		},
+	})
+	if err != nil {
+		log.Fatalf("Got error calling GetItem: %s", err)
+	}
+
+	item := Item{}
+
+	err = dynamodbattribute.UnmarshalMap(result.Item, &item)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to unmarshal Record, %v", err))
+	}
+
+	fmt.Println("Found item:")
+	fmt.Println("Year:  ", item.Year)
+	fmt.Println("Title: ", item.Title)
+	fmt.Println("Plot:  ", item.Plot)
+	fmt.Println("Rating:", item.Rating)
+}
